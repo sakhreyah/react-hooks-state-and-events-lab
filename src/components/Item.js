3f1c75f2a3c2ef08eef9e61e235abd4435c7a8f5
Musab Sakhreyah <mosabkhaled1998@gmail.com>
2023-10-29 15:05:52 +0300

    completed assignment

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
 
 function Item({ name, category }) {
+  const [isAdded, setIsAdded] = useState(false)
+  const addClass = isAdded ? "in-cart" : ""
+
   return (
-    <li className="">
+    <li className={addClass}>
       <span>{name}</span>
       <span className="category">{category}</span>
-      <button className="add">Add to Cart</button>
+      <button className={isAdded ? "remove" : "add"} onClick={() => setIsAdded(!isAdded)}>{isAdded ? "Remove From Cart" : "Add to Cart"}</button>
     </li>
   );
 }
